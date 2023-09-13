89d6db2692b10078b94a334e00e3dd7fda802ec8
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-09-13 18:21:03 +0300

    chore(models): Implement Item model

---
+++ models/item.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+
+const ItemSchema = new mongoose.Schema({
+  name: { type: String, minlength: 3, maxlength: 100, required: true },
+  description: { type: String, minlength: 3, maxlength: 1000, required: true },
+  price: { type: Number, min: 0, required: true },
+  image: { type: String, unique: true, required: true },
+  quantity: { type: Number, default: 0 },
+  createdAt: { type: Date, default: () => Date.now },
+});
+
+ItemSchema.virtual('url').get(function url() {
+  return `/item/${this._id}`;
+});
+
+ItemSchema.virtual('priceFormatted').get(function priceFormatted() {
+  return `$${this.price.toFixed(2)}`;
+});
+
+ItemSchema.virtual('createdAtFormatted').get(function createdAtFormatted() {
+  return DateTime.fromJSDate(this.created).toLocaleString(DateTime.DATE_MED);
+});
+
+ItemSchema.virtual('imageSrc').get(function imageSrc() {
+  return `/images/${this.image}`;
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
