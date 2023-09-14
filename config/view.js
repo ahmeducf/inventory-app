3ce92db77281978c1908d11966cb1d8cb464ef42
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-09-14 18:46:48 +0300

    fix: Use process's cwd instead of __direname to determine basedir

--- config/view.js
+++ config/view.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
 exports.setViewEngine = (app) => {
-  app.set('views', path.join(__dirname, 'views'));
+  app.set('views', path.join(process.cwd(), 'views'));
   app.set('view engine', 'pug');
 };
