8e3009ef2602027d51d60c1eeffff37f2538bc39
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-09-16 22:40:48 +0300

    refactor: Relieve ratelimiter max requests

--- utils/rate_limit.js
+++ utils/rate_limit.js
@@ -1,7 +1,7 @@
 const RateLimit = require('express-rate-limit');
 
-// set up rate limiter: maximum of 100 requests per 15 minutes
+// set up rate limiter: maximum of 10000 requests per 15 minutes
 module.exports = RateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 10000, // limit each IP to 10000 requests per windowMs
 });
